58bf6f162e07e3acf100a3ba3245149213995ad7
VOLTA0007 <volta@gmail.com>
2024-03-10 11:46:57 +0200

    Commit

--- utils/Config.js
+++ utils/Config.js
@@ -1,17 +1,9 @@
-let domain;
+var domain;
 
-async function fetchDomain() {
-    if (process.env.NODE_ENV === 'production') {
-        // Use the domain from environment variable in production and append /api
-        domain = `${process.env.VERCEL_URL}/api`;
-    } else {
-        // Use localhost domain for development
-        domain = 'http://localhost:3000/api';
-    }
+if (process.env.NODE_ENV === 'production') {
+    domain= 'nutrifit-neon.vercel.app/api'
+} else {
+    domain= 'http://localhost:3000/api'
 }
-
-// Call the function to fetch the domain
-fetchDomain();
-
 
 export default domain;

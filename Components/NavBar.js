dae9dfe712cbf1522886007619abbdb65770fa81
VOLTA0007 <volta@gmail.com>
2024-03-10 12:32:10 +0200

    Commit

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect, useRef } from 'react'; 
 import styles from '../styles/NavBar.module.css';
 
 export default function NavBar() {
     const [showOverlay, setShowOverlay] = useState(false);
-    const [isXShape, setIsXShape] = useState(false); // New state to manage X shape
+    const [isXShape, setIsXShape] = useState(false); 
+    const sideMenuRef = useRef(null);
 
     const toggleOverlay = () => {
         setShowOverlay(!showOverlay);
-        setIsXShape(!isXShape); // Toggle X shape state
+        setIsXShape(!isXShape); 
     };
+
+    const handleOutsideClick = (event) => {
+        if (sideMenuRef.current && !sideMenuRef.current.contains(event.target)) {
+            setShowOverlay(false);
+            setIsXShape(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+        document.addEventListener('touchstart', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+            document.removeEventListener('touchstart', handleOutsideClick);
+        };
+    }, []);
 
     return (
         <>
@@ -25,8 +43,8 @@ export default function NavBar() {
                     </nav>
                     <div 
                         className={`${styles.sidemenu} ${showOverlay ? styles.active : ''}`}
+                        ref={sideMenuRef}
                     >
-                        {/* Side menu content */}
                     </div>
                 </div>
             </div>

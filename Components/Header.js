a29e2d403976f0293f5d8111a9771afd052b37bc
VOLTA0007 <volta@gmail.com>
2024-03-14 05:50:00 +0200

    Commit

--- Components/Header.js
+++ Components/Header.js
@@ -1,11 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Header.module.css';
 import NavBar from './NavBar';
+import NavbarDesktop from './NavbarDesktop';
 
 export default function Header() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Check if window is defined to ensure this code runs in the client-side only
+        if (typeof window !== 'undefined') {
+            const mobileCheck = /Mobi/i.test(navigator.userAgent);
+            setIsMobile(mobileCheck);
+        }
+    }, []);
+
+    console.log('isMobile:', isMobile);
+
     return (
         <div className={styles.header1}>
             <p className={styles.paragraph1}>Welcome To Nutri Fit</p>
-            <NavBar />
+            {isMobile ? <NavBar /> : <NavbarDesktop />}
         </div>
     );
 }
---
+++ Components/NavbarDesktop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarDesktop = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default NavbarDesktop

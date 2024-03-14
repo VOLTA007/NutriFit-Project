41aaba099259c3701af7138d3311c535bce3a3ba
VOLTA0007 <volta@gmail.com>
2024-03-14 06:02:30 +0200

    Commit

--- Components/Header.js
+++ Components/Header.js
@@ -7,14 +7,11 @@ export default function Header() {
     const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
-        // Check if window is defined to ensure this code runs in the client-side only
         if (typeof window !== 'undefined') {
             const mobileCheck = /Mobi/i.test(navigator.userAgent);
             setIsMobile(mobileCheck);
         }
     }, []);
-
-    console.log('isMobile:', isMobile);
 
     return (
         <div className={styles.header1}>
--- Components/NavbarDesktop.js
+++ Components/NavbarDesktop.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'; 
+import styles from '../styles/NavbarDesktop.module.css';
+import Link from 'next/link';
 
 const NavbarDesktop = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+        <>
+            <div className={styles.navcontainer}>
+                <div className={styles.logomenu}>
+                    <img className={styles.Logo} src='./Nutrifitlogo.jpg' alt='Logo'></img>
+                    <nav>
+                        <div 
+                            className={styles.sidemenu}>
+
+                            <ul className={`${styles.ull} flex flex-col items-center justify-center`}>
+                                    
+                                <li className={`${styles.li}`}>
+                                <Link href="/Home" className="flex items-center">Home
+                                    <span><img className="w-6" src='./icons8-home-48.png' 
+                                        alt="Home Icon"></img></span>
+                                </Link>
+                                </li>
+                                <li className={`${styles.li}`}>
+                                <Link href="/Login" className="flex items-center">Login
+                                    <span><img className="w-6" src='./icons8-login-91.png' 
+                                        alt="Login Icon"></img></span>
+                                </Link>
+                                </li>
+                                <li className={`${styles.li}`}>
+                                <Link href="/Signup" className="flex items-center">Signup
+                                    <span><img className="w-6" src='./icons8-sign-up-32.png' 
+                                        alt="Signup Icon"></img></span>
+                                </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </>
+    );
+  }
 
 export default NavbarDesktop

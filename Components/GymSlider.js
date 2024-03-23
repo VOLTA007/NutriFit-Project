a35d32edeb8928b201f3a81fb407b561f672444e
D-V-GITHUB <voltagaming2001@gmail.com>
2024-03-23 05:30:24 +0200

    Commit

--- Components/GymSlider.js
+++ Components/GymSlider.js
@@ -30,10 +30,10 @@ const GymSlider = ({ children: images }) => {
           {images}
         </div>
         <div>
-          <button onClick={handlePrevious}  className=" absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <button onClick={handlePrevious}  className="hidden absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
             <BsChevronCompactLeft size={30} />
           </button>
-          <button onClick={handleNext} className=" absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <button onClick={handleNext} className="hidden absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
             <BsChevronCompactRight size={30} />
           </button>
         </div>

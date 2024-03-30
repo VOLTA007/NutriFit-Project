2e812b73c754ab5c13defb91f4ff62c2ce1f3869
VOLTA0007 <volta@gmail.com>
2024-03-30 07:19:13 +0200

    Commit

--- Components/Login.js
+++ Components/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'; 
+import { useState, useEffect } from 'react'; 
 import styles from '@/styles/Login.module.css';
 import axios from 'axios';
 import domain from '@/utils/Config';
@@ -6,15 +6,17 @@ import { useRouter } from 'next/router';
 import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link';
 
+
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [notificationVisible, setNotificationVisible] = useState(false);
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('')
     const router = useRouter();
     const session = useSession();
     const status = session.status;
+    console.log(session);
 
     if(status === 'authenticated'){
         router.push('/Home')
@@ -28,13 +30,13 @@ export default function Login() {
             const { success } = response.data;
     
             if (success) {
-                setMessage('Logged in Successfully');
-                setNotificationVisible(true);
+                setMessage('Logged in Successfully'); // Set success message
+                setNotificationVisible(true); // Show notification bar
                 setTimeout(async () => {
                   setNotificationVisible(false);
                   await signIn('credentials', { email, password }); 
                 }, 4250);
-            } else {
+              } else {
                 setNotificationVisible(true);
                 setTimeout(() => {
                     setNotificationVisible(false);
@@ -47,63 +49,72 @@ export default function Login() {
                 setTimeout(() => {
                     setNotificationVisible(false);
                 }, 5000);
-                setMessage('Invalid Email or Password. Please try again.');
+                setMessage('Invalid Email or Password. Please try again.'); // Set error message
             } else {
                 setNotificationVisible(true);
                 setTimeout(() => {
                     setNotificationVisible(false);
                 }, 5000);
                 console.error('Error logging in:', error);
-                setMessage('An error occurred while logging in. Please try again later.');
+                setMessage('An error occurred while logging in. Please try again later.'); // Set error message
             }
         } finally {
-            setTimeout(() => {
+            // Delay for 5000ms before disabling isLoading state and signing in
+            setTimeout(async () => {
                 setIsLoading(false); 
             }, 6000);
         }
+        
     }
 
     return (
         <>
-            <form onSubmit={handleFormSubmit} className={`${styles.loginForm} mx-auto max-w-[400px] w-45 h-[400px]`}>
-                <h1 className="text-center">Login</h1>
+            <form onSubmit={handleFormSubmit} className="mx-auto max-w-[400px] w-45 h-[400px] bg-[#edfb14] rounded-xl
+                 grid grid-rows-4 mt-[120px] m-8 p-5 gap-6">
+
+                <h1 className="place-self-center text-center">Login <span className="flex items-center justify-center"><img src='./icons8-login-90.png'
+                className="w-8 h-auto"></img></span>Welcome To Nutrifit :)</h1>
                 <div className={styles.inputcontainer}>
-                    <input 
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="email" 
-                        value={email}
-                        className={`${styles.input} rounded-md p-2`}
-                        pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-                        required
-                    />
-                    <img src="./email.png" className={styles.icon} alt="Email icon" />
+                <input 
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email" 
+                    value={email}
+                    className={`${styles.input} rounded-md p-2 dark:bg-[#0d121b] dark:text-gray-50 ${isLoading ? styles.disabled : ''}`} 
+                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                    required
+                />
+                <img src="./email.png" className={styles.icon} style={{ maxHeight: '20px', maxWidth: '20px' }} alt="Email icon" />
                 </div>
+
                 <div className={styles.inputcontainer}>
-                    <input 
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password" 
-                        value={password} 
-                        className={`${styles.input} rounded-md p-2`}
-                        disabled={isLoading} />
-                    <img src="./padlock.png" className={styles.icon} alt="Password icon" />
+                <input 
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password" 
+                    value={password} 
+                    className={`${styles.input} rounded-md dark:bg-[#0d121b] dark:text-gray-50 p-2 ${isLoading ? styles.disabled : ''}`}
+                    disabled={isLoading} />
+                <img src="./padlock.png" className={styles.icon} style={{ maxHeight: '20px', maxWidth: '20px' }} alt="Password icon" />
                 </div>
+
                 <div>
-                    <p>Create Account Here!{' '}
+                    <p>Create Account Here! {' '}
                         <Link href="/Signup">
-                            Signup
+                             Signup
                         </Link>
                     </p>
                 </div>
+                    
                 <button 
                     className={`${styles.but} ${isLoading ? styles.loading : ''}`} 
                     type="submit" 
                     disabled={isLoading}>
                     {isLoading ? (<div className={styles.loader}></div>) : ('Login')}
                 </button>
+
             </form>
 
             {notificationVisible && (
-                <div className={`${styles.notification} ${notificationVisible ? styles.show : ''}`}>
+                <div className={`${styles.notification} ${notificationVisible  ? styles.show : ''}`}>
                     {message}
                     <div className={styles.bar}></div>
                 </div>

ca2f233b84c99cf278d156970e13cc5b3984c946
VOLTA0007 <volta@gmail.com>
2024-03-30 05:57:38 +0200

    Commit

--- Components/MainLayout.js
+++ Components/MainLayout.js
@@ -1,37 +1,34 @@
-import { SessionProvider } from "next-auth/react"
-import Footer from "./Footer"
-import Header from "./Header"
+import { SessionProvider } from "next-auth/react";
+import Footer from "./Footer";
+import Header from "./Header";
 import NavBarDesktop from './NavbarDesktop';
 import NavBar from './NavBar';
 import { useEffect, useState } from "react";
 
-
 export default function MainLayout(props) {
-
     const [isMobile, setIsMobile] = useState(null);
 
-  useEffect(() => {
-    const checkIsMobile = () => {
-      const isMobileUserAgent = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-      setIsMobile(isMobileUserAgent);
-    };
+    useEffect(() => {
+        const checkIsMobile = () => {
+            const isMobileUserAgent = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+            setIsMobile(isMobileUserAgent);
+        };
 
-    checkIsMobile();
-  }, []);
+        checkIsMobile();
+    }, []);
 
-  if (isMobile === null) {
-    return <div></div>; 
-  }
+  
+
 
     return (
         <>
-            <SessionProvider>
-                <Header />
-                {isMobile ? <NavBar /> : <NavBarDesktop />}
-                <div>{props.children}</div>
-                <div style={{paddingTop: '120px'}}></div>
-                <Footer />
-            </SessionProvider>
+          <SessionProvider>
+              <Header />
+              {isMobile ? <NavBar /> : <NavBarDesktop />}
+              <div>{props.children}</div>
+              <div style={{paddingTop: '120px'}}></div>
+              <Footer />
+          </SessionProvider>
         </>
-    )
+    );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import MainLayout from '@/Components/MainLayout';
 import styles from '@/styles/globals.css';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import Home from '@/pages/Home/index';
-
+import { SessionProvider } from 'next-auth/react';
+import MainLayout from '@/Components/MainLayout';
 
 
 export default function App({ Component, pageProps }) {
-
   const router = useRouter();
 
   useEffect(() => {
@@ -16,10 +15,29 @@ export default function App({ Component, pageProps }) {
       router.push('/Home'); // Redirect to /Home on server-side (prevents client-side loop)
     }
   }, []);
- 
-  return <MainLayout>
-  <Component {...pageProps} />
-  </MainLayout>
+
+
+  return<MainLayout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.pathname}>
+          <Component {...pageProps} />
+
+          <motion.div
+            className="absolute top-0 left-0 w-full h-screen bg-black origin-bottom z-40 rounded-t-[100px]"
+            initial={{ scaleY: 0 }}
+            animate={{ scaleY: 0 }}
+            exit={{ scaleY: 1.5 }}
+            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+          />
+
+          <motion.div
+            className="absolute top-0 left-0 w-full h-screen bg-black origin-top z-40 rounded-b-[100px]"
+            initial={{ scaleY: 1.5 }}
+            animate={{ scaleY: 0 }}
+            exit={{ scaleY: 0 }}
+            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+          />
+        </motion.div>
+      </AnimatePresence>
+    </MainLayout>
 }
-
-

bc43a1412365ea1384cc0285c7ffdd8fdab3f18f
VOLTA0007 <volta@gmail.com>
2024-04-01 00:10:44 +0200

    Commit

--- Components/Userwelcome.js
+++ Components/Userwelcome.js
@@ -1,62 +1,66 @@
 import { motion } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 
-
 const UserWelcome = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         // Set a timeout to change the loading state after 2 seconds
         const timeoutId = setTimeout(() => {
-          setIsLoading(false);
+            setIsLoading(false);
         }, 2000);
-    
+
         // Clear the timeout if the component unmounts or loading state changes
         return () => clearTimeout(timeoutId);
-      }, []);
-    
+    }, []);
 
-  return (
-    <>
-    <div style={{ paddingTop: "60px" }}></div>
-    <motion.div className='h-full' initial={{y:"200vh"}} animate={{y:"0%"}} exit={{y:"-200vh"}} transition={{duration:0.5}}>
-
-    {isLoading ? (
-        <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-            <div class="animate-pulse flex space-x-4">
-                <div class="rounded-full bg-slate-700 h-10 w-10"></div>
-                <div class="flex-1 space-y-6 py-1">
-                <div class="h-2 bg-slate-700 rounded"></div>
-                <div class="space-y-3">
-                    <div class="grid grid-cols-3 gap-4">
-                    <div class="h-2 bg-slate-700 rounded col-span-2"></div>
-                    <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+    return (
+        <>
+            <div style={{ paddingTop: "60px" }}></div>
+            <motion.div className='h-full' initial={{ y: "200vh" }} animate={{ y: "0%" }} exit={{ y: "-200vh" }} transition={{ duration: 0.5 }}>
+                {isLoading ? (
+                    <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                        <div className="animate-pulse flex space-x-4">
+                            <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+                            <div className="flex-1 space-y-6 py-1">
+                                <div className="h-2 bg-slate-700 rounded"></div>
+                                <div className="space-y-3">
+                                    <div className="grid grid-cols-3 gap-4">
+                                        <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                                        <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+                                    </div>
+                                    <div className="h-2 bg-slate-700 rounded"></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="h-2 bg-slate-700 rounded"></div>
-                </div>
-                </div>
-            </div>
-        </div>
-    ) : (
-    <div className='flex justify-center items-center gap-6'>
-        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-            <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-        </div>
+                ) : (
+                    <div className='flex justify-center items-center'>
+                    <nav className="flex px-5 py-3 justify-center items-center text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb" style={{ width: '80px' }}>
+                        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                            <li key="home" className="inline-flex items-center">
+                                <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                    <svg className="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
+                                    </svg>
+                                    Home
+                                </a>
+                            </li>
+                        </ol>
+                    </nav>
+                    </div>
 
-        <p className='dark:text-white'>Welcome, Username 😊</p>
-        
-        
-    </div>
-    )}
-    
-    </motion.div>
-    </>
-  );
+                )}
+                <div style={{ paddingTop: "20px" }}></div>
+                <div className='flex justify-start items-center gap-6 ml-10'>
+                    <div className="relative w-16 h-16 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                        <svg className="absolute w-19 h-19 text-gray-400 -left-0 top-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+                    </div>
+                    <p className='dark:text-white text-lg'>Welcome, Username 😊</p>
+                </div>
+            </motion.div>
+        </>
+    );
 }
-
-{/* // <div className="relative">
-        //   <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="User profile picture" />
-        //   <span className="top-0 left-7 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
-        // </div> */}
 
 export default UserWelcome;

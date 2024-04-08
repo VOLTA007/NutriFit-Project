5ce1ff0c6280aa12cf53c1e070608b04387f6d78
VOLTA0007 <volta@gmail.com>
2024-04-08 12:03:21 +0200

    Commit

--- pages/Pricing/index.js
+++ pages/Pricing/index.js
@@ -155,7 +155,7 @@ const Pricing = () => {
                         </p>
                         <button
                             type="button"
-                            className=" mt-8 text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                            className="mt-8 text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                             onClick={() => setisclicked(true)}
                         >
                             Subscribe Now
@@ -166,7 +166,7 @@ const Pricing = () => {
 
             {isclicked ? (
                 <motion.div
-                    className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50"
+                    className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50"
                     initial={{ y: '200vh' }}
                     animate={{ y: '0%' }}
                     exit={{ y: '-200vh' }}

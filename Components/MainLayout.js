dab612fc202fb54d50166287373abd4125856e6e
VOLTA0007 <volta@gmail.com>
2024-04-08 14:14:56 +0200

    Commit

--- Components/MainLayout.js
+++ Components/MainLayout.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { usePathname } from 'next/navigation'
+import { NextUIProvider } from '@nextui-org/react'
 
 export default function MainLayout({ children }) {
     const router = useRouter()
@@ -31,31 +32,39 @@ export default function MainLayout({ children }) {
 
 
     return (
-        <SessionProvider>
-            <AnimatePresence mode="wait">
-                <motion.div key={pathname}>
-                    <motion.div
-                        className="absolute top-0 left-0 w-full h-screen bg-black origin-bottom z-40 rounded-t-[100px]"
-                        initial={{ scaleY: 0 }}
-                        animate={{ scaleY: 0 }}
-                        exit={{ scaleY: 1.5 }}
-                        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-                    />
+        <NextUIProvider>
+            <SessionProvider>
+                <AnimatePresence mode="wait">
+                    <motion.div key={pathname}>
+                        <motion.div
+                            className="absolute top-0 left-0 w-full h-screen bg-black origin-bottom z-40 rounded-t-[100px]"
+                            initial={{ scaleY: 0 }}
+                            animate={{ scaleY: 0 }}
+                            exit={{ scaleY: 1.5 }}
+                            transition={{
+                                duration: 1,
+                                ease: [0.22, 1, 0.36, 1],
+                            }}
+                        />
 
-                    <motion.div
-                        className="absolute top-0 left-0 w-full h-screen bg-black origin-top z-40 rounded-b-[100px]"
-                        initial={{ scaleY: 1.5 }}
-                        animate={{ scaleY: 0 }}
-                        exit={{ scaleY: 0 }}
-                        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-                    />
-                    <Header />
-                    {isMobile ? <NavBar /> : <NavBarDesktop />}
-                    <div>{children}</div>
-                    <div style={{ paddingTop: '120px' }}></div>
-                    <Footer />
-                </motion.div>
-            </AnimatePresence>
-        </SessionProvider>
+                        <motion.div
+                            className="absolute top-0 left-0 w-full h-screen bg-black origin-top z-40 rounded-b-[100px]"
+                            initial={{ scaleY: 1.5 }}
+                            animate={{ scaleY: 0 }}
+                            exit={{ scaleY: 0 }}
+                            transition={{
+                                duration: 1,
+                                ease: [0.22, 1, 0.36, 1],
+                            }}
+                        />
+                        <Header />
+                        {isMobile ? <NavBar /> : <NavBarDesktop />}
+                        <div>{children}</div>
+                        <div style={{ paddingTop: '120px' }}></div>
+                        <Footer />
+                    </motion.div>
+                </AnimatePresence>
+            </SessionProvider>
+        </NextUIProvider>
     )
 }
